d0ae1530a986f15a7ff7c06a78603a4cda722d87
Ponter Chia <dozeuto@gmail.com>
2026-05-26 21:50:28 +0800

    initial project

---
+++ SwiftUIDisplay/SwiftUIDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDisplayApp.swift
+//  SwiftUIDisplay
+//
+//  Created by Pongt Chia on 26/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIDisplayTests/SwiftUIDisplayTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDisplayTests.swift
+//  SwiftUIDisplayTests
+//
+//  Created by Pongt Chia on 26/5/26.
+//
+
+import Testing
+
+struct SwiftUIDisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
